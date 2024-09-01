2d64cfd58455c1d1b40cac8423b924794367f38b
Ernest Kumashie <mauly.dev@gmail.com>
2024-09-01 18:07:38 +0000

    profile updated

--- health_profile/apps.py
+++ health_profile/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class HealthProfileConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'health_profile'
+    
+    def ready(self):
+        import health_profile.signals
--- health_profile/models.py
+++ health_profile/models.py
@@ -4,10 +4,11 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
-    age = models.IntegerField()
-    occupation = models.CharField(max_length=50)
-    avg_screen_time = models.DecimalField(max_digits=10, decimal_places=2, help_text='Hours')
+    age = models.IntegerField(blank=True, null=True)
+    occupation = models.CharField(max_length=50, blank=True, null=True)
+    avg_screen_time = models.DecimalField(max_digits=10, decimal_places=2, help_text='Hours', default=0.0)
     health_story = models.TextField(blank=True, default="no-story")
+    health_response = models.JSONField(blank=True, null=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     
