cc3a6c5ef2c5121217b65297c34e0ce7f7203c09
Ernest Kumashie <mauly.dev@gmail.com>
2024-09-20 12:50:11 +0000

    ai processing

--- advice/views.py
+++ advice/views.py
@@ -1,11 +1,12 @@
 from rest_framework import viewsets
 from .models import Advice
 from .serializers import AdviceSerializer
-
+from django_filters.rest_framework import DjangoFilterBackend
 
 
 
 class AdviceViewSet(viewsets.ModelViewSet):
     queryset = Advice.objects.all()
     serializer_class = AdviceSerializer
+    filter_backends = [DjangoFilterBackend]
     filterset_fields = ['profile__user__username']
